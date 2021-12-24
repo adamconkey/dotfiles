0ede2b143d460948520aaab8b6ae82b7bde30c77
Adam Conkey <adam.conkey@utah.edu>
2021-12-24 11:47:03 -0700

    Update with working script

--- .dotfiles_setup/install_dotfiles.sh
+++ .dotfiles_setup/install_dotfiles.sh
@@ -1,16 +1,33 @@
 #!/usr/bin/env bash
 
-git clone --bare git@github.com:adamconkey/dotfiles.git $HOME/.dotfiles
+set -u  # exit on unset vars
+
 function dotfiles {
    git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
-mkdir -p .dotfiles-backup
-dotfiles checkout
+
+
+DOTFILES_BACKUP_DIR="$HOME/.dotfiles-backup"
+
+# Clone bare git repo to hidden folder in home
+git clone --bare git@github.com:adamconkey/dotfiles.git $HOME/.dotfiles
+if [ $? -ne 0 ]; then
+    # TODO currently just exits if the dir already exists, but could possibly
+    # do something else like backup and delete
+    exit 1
+fi
+
+# Try to checkout the files, backup any that exist already
+cd $HOME
+mkdir -p "$DOTFILES_BACKUP_DIR"
+dotfiles checkout 2>/dev/null
 if [ $? = 0 ]; then
-  echo "Checked out dotfiles";
-  else
-    echo "Backing up pre-existing dotfiles";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
-fi;
-dotfiles checkout
-dotfiles config status.showUntrackedFiles no
+    echo -e "\nChecked out dotfiles";
+else
+    echo -e "\nBacking up pre-existing dotfiles";
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $DOTFILES_BACKUP_DIR/{} 2>/dev/n\
+ull
+fi
+dotfiles checkout -f  # Force so it doesn't worry about moving script and README.md
+dotfiles config status.showUntrackedFiles no  # Ignore everything else in home dir
+echo -e "\nTracked dotfiles should be in home directory now\n"
